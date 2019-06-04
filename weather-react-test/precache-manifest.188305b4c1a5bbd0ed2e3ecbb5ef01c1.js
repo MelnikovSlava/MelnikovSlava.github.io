e30d84e68450097aea0bb2eada98dc7b7c333ba4
MelnikovSlava <jefferi@ya.ru>
2019-06-04 21:40:54 +0300

    Add weather-react-test

---
+++ weather-react-test/precache-manifest.188305b4c1a5bbd0ed2e3ecbb5ef01c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e07dbbfd7bf58b1ab27177ab371c888",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5eb4c387b21f7622fff0",
+    "url": "/static/css/main.4ce2e5c8.chunk.css"
+  },
+  {
+    "revision": "f07d41e9a20bfaf6995a",
+    "url": "/static/js/2.7f104661.chunk.js"
+  },
+  {
+    "revision": "5eb4c387b21f7622fff0",
+    "url": "/static/js/main.c99916f6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
